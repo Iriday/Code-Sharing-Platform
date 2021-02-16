5ddcda7768272d1c4b2b75a1880f138c661b7614
Igor <GI_Something@protonmail.com>
2021-02-16 12:22:12 +0200

    feat: Partially impl "get /code/new" (returns page(form) that allows to input, and send new code)

--- src/main/java/com/spring_project/code_sharing_platform/code/CodeController.java
+++ src/main/java/com/spring_project/code_sharing_platform/code/CodeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class CodeController {
@@ -38,5 +39,10 @@ public class CodeController {
                 .ok()
                 .header("Content-Type", "text/html")
                 .body(codeService.getCodeAsHTML());
+    }
+
+    @GetMapping("/code/new")
+    ModelAndView getInputCodePage() {
+        return new ModelAndView("input_code");
     }
 }
